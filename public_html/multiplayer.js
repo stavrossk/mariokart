bea11b19c09b884ec1b7b117ae29ce61c311fcfa
Stavos Skamagkis <stavrossk@gmail.com>
2013-08-22 12:43:29 +0300

    Improved code readability.

--- public_html/multiplayer.js
+++ public_html/multiplayer.js
@@ -1,69 +1,200 @@
-onDomReady(function() {
-    var lastMove, lastMoveSent, player,
-        playerProps = ["rotation", "rotinc", "rotincdir", "speed", "speedinc", "x", "y"],
-        timer = null;
-        
-    var playerCount = MarioKart.getPlayerCount();
+onDomReady
+(
 
-    var socket = io.connect();
-    
-    socket.on("connect", function(e) {
-        console.log("connected!");
-    });
-    
-    socket.on("disconnect", function() {
-        //
-    });
-    
-    socket.on("message", function(msg) {
-        msg = JSON.parse(msg);
-        if (playerCount < 2 && msg.type != "playerCount")
-            return;
-        //console.log("message received: ", msg.type, msg.player, msg);
-        switch (msg.type) {
-            case "playerJoin":
-                MarioKart.addPlayer(msg.player);
-                break;
-            case "playerMapSelect":
-                MarioKart.setMap(msg.map);
-                break;
-            case "startRace":
-                MarioKart.gotoGameStart();
-                break;
-            case "playerMove":
-                MarioKart.movePlayer(msg.player, msg);
-                break;
-            case "playerCount":
-                MarioKart.setPlayerCount(msg.count);
-                break;
-            case "playerLeave":
-                MarioKart.reset();
-                break;
-        }
-    });
-    
-    function buildMoveObject(oPlayer) {
-        var obj = {type: "playerMove", player: player};
-        playerProps.forEach(function(prop) {
-            obj[prop] = oPlayer[prop];
-        });
-        return obj;
-    }
-    
-    MarioKart.on("playerMove", function(oPlayer) {
-        if (playerCount < 2)
-            return;
-        lastMove = buildMoveObject(oPlayer);
-        if (timer)
-            return;
-        timer = setInterval(function() {
-            if (lastMoveSent === lastMove)
-                return;
-            lastMoveSent = lastMove;
-            socket.json.send(lastMove);
-        }, 200);
-    });
-    
+    function()
+    {
+
+        var lastMove,
+            lastMoveSent,
+            player,
+
+            playerProps =
+                [
+
+                    "rotation",
+                    "rotinc",
+                    "rotincdir",
+                    "speed",
+                    "speedinc",
+                    "x",
+                    "y"
+
+                ],
+
+            timer = null;
+        
+            var playerCount
+                = MarioKart
+                .getPlayerCount();
+
+
+            var socket
+                = io
+                .connect();
+
+
+            socket.on
+            (
+                "connect",
+
+                function(e)
+                {
+
+                            console.log("connected!");
+
+                        }
+
+            );
+
+
+            socket.on
+            (
+
+                "disconnect",
+
+                function()
+                {
+
+                    //
+
+                }
+
+            );
+
+
+            socket.on
+            (
+
+                "message",
+
+                function(msg )
+                {
+
+                msg = JSON.parse(msg);
+
+                 if (playerCount < 2 && msg.type != "playerCount")
+                    return;
+
+                 //console.log("message received: ", msg.type, msg.player, msg);
+
+                 switch (msg.type)
+                 {
+
+                    case "playerJoin":
+                        MarioKart
+                            .addPlayer(msg.player);
+                    break;
+
+                    case "playerMapSelect":
+                        MarioKart
+                            .setMap(msg.map);
+                    break;
+
+                    case "startRace":
+                        MarioKart
+                            .gotoGameStart();
+                    break;
+
+                    case "playerMove":
+                        MarioKart
+                            .movePlayer(msg.player, msg);
+                    break;
+
+                    case "playerCount":
+                        MarioKart
+                            .setPlayerCount(msg.count);
+                    break;
+
+                    case "playerLeave":
+                        MarioKart
+                            .reset();
+                    break;
+
+                 }
+
+
+            }
+
+            );
+
+
+
+            function buildMoveObject(oPlayer)
+            {
+
+                var obj =
+                {
+
+                    type: "playerMove",
+
+                    player: player
+
+                };
+
+                playerProps
+                    .forEach
+                    (
+
+                        function(prop)
+                        {
+
+                            obj[prop] = oPlayer[prop];
+
+                        }
+
+                    );
+
+                return obj;
+
+            }
+
+
+
+            MarioKart
+                .on
+            (
+
+                "playerMove",
+
+                function(oPlayer)
+                {
+
+                    if (playerCount < 2)
+                        return;
+
+                    lastMove = buildMoveObject(oPlayer);
+
+                    if (timer)
+                        return;
+
+                    timer = setInterval
+                    (
+
+                        function()
+                        {
+
+                        if (lastMoveSent === lastMove)
+                            return;
+
+                        lastMoveSent = lastMove;
+
+                        socket
+                            .json
+                            .send(lastMove);
+
+
+                    },
+
+                        200
+
+                    );
+
+                }
+
+            );
+
+
+
     MarioKart.on("playerSelect", function(msg) {
         if (playerCount < 2)
             return;
--- server.js
+++ server.js
@@ -1,18 +1,58 @@
+
 var Connect = require("connect");
+
 var SocketIO = require("socket.io");
 
-var server = Connect.createServer(
-    Connect.static(__dirname + "/public_html"),
-    Connect.static(__dirname + "/Socket.IO/dist")
-);
-// start the server
-server.listen(process.env.C9_PORT, "0.0.0.0");
 
-var Server = require("./server/index");
+var server = Connect.createServer
+    (
+
+        Connect.static(__dirname + "/public_html"),
+
+        Connect.static(__dirname + "/Socket.IO/dist")
+
+    );
+
+// start the server
+
+server.listen
+    (
+
+        process.env.C9_PORT,
+        "0.0.0.0"
+
+    );
+
+
+var Server = require
+    (
+
+        "./server/index"
+
+    );
+
 var playerServer = new Server();
 
-var socket = SocketIO.listen(server);
-socket.sockets.on("connection", function(client){
-    // new client is here!
-    playerServer.addClient(client);
-});
+
+var socket = SocketIO
+    .listen(server);
+
+
+socket
+    .sockets
+    .on
+    (
+
+        "connection",
+
+        function(client)
+        {
+
+            // new client is here!
+
+            playerServer
+                .addClient(client);
+
+        }
+
+    );
